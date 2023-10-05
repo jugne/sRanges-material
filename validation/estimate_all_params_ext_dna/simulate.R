41ba800a56d5a537467e5eabe963e8a3e6640349
jugne <ugne.stolz@bsse.ethz.ch>
2023-10-05 15:19:06 +0200

    added comments

--- validation/estimate_all_params_ext_dna/simulate.R
+++ validation/estimate_all_params_ext_dna/simulate.R
@@ -8,22 +8,28 @@ library(stringr)
 library(beastio)
 library(FossilSim)
 library(R.utils)
-source("/Users/jugne/Documents/Source/beast2.7/sRanges-material/validation/estimate_all_params_priors/helper.R")
+## change paths if needed
+source("~/Documents/Source/beast2.7/sRanges-material/validation/estimate_all_params_ext_dna/helper.R")
 
+## This overrides the tree simulation function, so that too big (>1000 nodes) tree are rejected.
+## This is done since they are too big for quick BEAST2 analysis
 tmpfun <- get("sim2.bd.origin", envir = asNamespace("TreeSim"))
 environment(sim2.bd.origin2) <- environment(tmpfun)
 attributes(sim2.bd.origin2) <- attributes(tmpfun)  # don't know if this is really needed
 assignInNamespace("sim2.bd.origin", sim2.bd.origin2, ns="TreeSim")
-
+## helper function 
 '%notin%' <- Negate('%in%')
-wd <- "~/Documents/Source/beast2.7/sRanges-material/validation/estimate_all_params_priors/"
-templates_dir <- "~/Documents/Source/beast2.7/sRanges-material/validation/estimate_all_params_priors/templates/"
+
+## change paths if needed
+wd <- "~/Documents/Source/beast2.7/sRanges-material/validation/estimate_all_params_ext_dna/"
+templates_dir <- "~/Documents/Source/beast2.7/sRanges-material/validation/estimate_all_params_ext_dna/templates/"
+beast_dir <- "'/Applications/BEAST\ 2.7.1/bin/beast'"
 
 
 set.seed(5647829)
 
-### Simulation parameters
 
+### Parameter transformation functions
 f_lambda <- function(diversification, turnover){
   return(diversification/(1-turnover))
 }
@@ -36,7 +42,7 @@ f_psi <- function(mu, sampling_prop){
   return(mu*sampling_prop/(1-sampling_prop))
 }
 
-
+## Set initial params to null, origin will be fixed (but still estimated)
 div_rate = NULL
 turnover = NULL
 sampling_prop = NULL
@@ -47,8 +53,9 @@ psi = NULL
 redraws = 0
 origin = 4
 
+## function to redraw parameters from their posteriors (uniform in all cases here)
 redraw_params <- function(){
-  div_rate <<- runif(1, 0.7, 0.9)#rlnorm(1, meanlog = -3.5, sdlog = 1.5)
+  div_rate <<- runif(1, 0.7, 0.9)
   turnover <<- runif(1, 0.2, 0.8)
   sampling_prop <<- runif(1, 0.2, 0.8)
   sampl_extant_prob <<- runif(1, 0.7, 1.)
@@ -56,15 +63,22 @@ redraw_params <- function(){
   lambda <<- f_lambda(div_rate, turnover)
   mu <<- f_mu(turnover, lambda)
   psi <<- f_psi(mu, sampling_prop)
-  # origin <<- runif(1, 40, 80)
 }
 
-
+# number of trees to simulate
 ntrees = 200
 
-min_ext_samples = 5
-max_samples = 1000
 
+## The following min and max values are set to ensure a small enough tree.
+## If they are violated, the simulation will be rejected and new parameter values redrawn.
+## This is a bit problematic, since we do not account in any way for rejected simulations and parameter combinations when doing inference.
+## In inference we only condition on survival (so not on min or max number of samples).
+
+# min for extant samples and max for total nodes
+min_ext_samples = 5
+max_nodes = 1000
+
+# min and max value for fossils
 min_fossils = 0
 max_fossils = 10000
 
@@ -86,12 +100,12 @@ while(length(trees) < ntrees) {
   redraws = redraws+1
   tree_tmp <- TreeSim::sim.bd.age(origin, 1, lambda, mu)
   
-  if (length(tree_tmp[[1]])==1 || tree_tmp[[1]]$Nnode>1000){
-    next
+  if (length(tree_tmp[[1]])==1 || tree_tmp[[1]]$Nnode>max_nodes){
+    next # reject and redraw parameters
   }
   mrca = max(ape::node.depth.edgelength(tree_tmp[[1]]))
   if (length(which((mrca - ape::node.depth.edgelength(tree_tmp[[1]])) < 1e-7)) < min_ext_samples){
-    next
+    next # reject and redraw parameters
   }
   
   taxonomy_tmp = sim.taxonomy(tree_tmp[[1]], beta=0, lambda.a = 0)
@@ -103,7 +117,7 @@ while(length(trees) < ntrees) {
   n_ext <- length(which((mrca - ape::node.depth.edgelength(tree_after_rho)) < 1e-7))
   n_fossils <- length(tree_after_rho$tip.label) - n_ext
   if (n_ext < min_ext_samples || n_fossils < min_fossils || n_fossils > max_fossils){
-    next
+    next # reject and redraw parameters
   }
   
   trees = c(trees, tree_tmp)
@@ -119,14 +133,14 @@ while(length(trees) < ntrees) {
   
 }
 
+## Now create simulation xmls for each tree and use it to simulate DNA and morphological
+## alignments at all occurences. Then remove DNA for extinct occurences.
+
 for (i in 1:ntrees){
   
-  beast_tree = beast_trees[[i]]#beast.fbd.format(trees[[i]],fossils[[i]], rho=true_rates$rho[i], digits=16)
+  beast_tree = beast_trees[[i]]
   true_rates$tree[i] = beast_tree
   tmp_tree = ape::read.tree(text=beast_tree)
-  # true_rates$mrca[i] = max(ape::node.depth.edgelength(tmp_tree))
-  # true_rates$n_samples[i] = length(tmp_tree$tip.label)
-  # true_rates$n_extant[i] = sum(true_rates$mrca[i]-ape::node.depth.edgelength(tmp_tree)<1e-10)
   sample_times = ape::node.depth.edgelength(tmp_tree)
   sample_times = max(sample_times) - sample_times
   sample_times_round = round(sample_times,15)
@@ -190,15 +204,15 @@ for (i in 1:ntrees){
   sim  <- gsub(pattern = "<insertSequence/>",
                replace = taxon_extant_str, x = sim)
   
-  
+  ##### run beast on the xml simulating DNA data
   sim_dir <- paste0(wd, "run_", i, "/sim")
   dir.create(sim_dir, recursive=T)
   setwd(sim_dir)
   writeLines(sim, con='sRanges_simDNA.xml')
-  cmd <- "'/Applications/BEAST\ 2.7.1/bin/beast' -seed 42 sRanges_simDNA.xml"
+  cmd <- paste0(beast_dir," -seed 42 sRanges_simDNA.xml")
   system(cmd)
   
-  
+  ## remove DNA from extinct occurences
   extinct <- taxon[which(taxon %notin% taxon_extant)]
   dna_sim <- readLines("simulated_dna_alignment.xml")
   idx  <- grep("<data id=", dna_sim)
@@ -224,7 +238,6 @@ for (i in 1:ntrees){
   taxon_extant_str = c()
   for (tx in taxon){
     taxon_extant_str <- c(taxon_extant_str, paste0("<sequence spec='Sequence' taxon='",tx,"' value='?'/>"))
-    # taxon_extant_str<- paste0(taxon_extant_str, "<sequence spec='Sequence' taxon='",tx,"' value='?'/>", collapse = "\n")
   }
   taxon_extant_str <- paste0(taxon_extant_str, collapse = "\n\t\t\t")
   
@@ -243,9 +256,9 @@ for (i in 1:ntrees){
   sim  <- gsub(pattern = "<insertMorphSequence/>",
                replace = taxon_str, x = sim)
   
-  
+  ##### run beast on the xml simulating morphological data
   writeLines(sim, con='sRanges_simMorph.xml')
-  cmd <- "'/Applications/BEAST\ 2.7.1/bin/beast' -seed 42 sRanges_simMorph.xml"
+  cmd <- paste0(beast_dir," -seed 42 sRanges_simMorph.xml")
   system(cmd)
   
   morph_sim <- readLines("simulated_morph_alignment.xml")
@@ -256,7 +269,7 @@ for (i in 1:ntrees){
   morph_sim  <- gsub(pattern = ",",
                      replace = "", x = morph_sim)
   
-  
+  ####### now create inference xmls, with the previosuly simulated data
   sim <- readLines(paste0(templates_dir,"sRanges_inference_template.xml"))
   sim  <- gsub(pattern = "<insertStartMorphData/>",
                replace = paste0(morph_sim, collapse = "\n"), x = sim)
